5bc5d34b6dc1f6327d44852c5613f23c0ca25727
daniele58 <60782906+daniele58@users.noreply.github.com>
2020-03-11 23:27:36 +0100

    Update suo.js

--- suo.js
+++ suo.js
@@ -1,4 +1,4 @@
-var body = ['scemo/a', 'coglione/a', 'stuppagghiaro/a', 'una merdaccia', 'stronzo/a','cazzone/a', 'insensibile', 'incosciente', 'negligente', 'menomato', 'ritardato/a', 'giustificato per la 104', 'portatore di COVID-19', 'il paziente 0', 'stato a Bergamo', 'sei di Codogno', 'in quarantena'];
+var body = ['scemo/a', 'coglione/a', 'stuppagghiaro/a', 'una merdaccia', 'stronzo/a','cazzone/a', 'insensibile', 'incosciente', 'negligente', 'menomato', 'ritardato/a', 'giustificato per la 104', 'portatore di COVID-19', 'il paziente 0', 'stato a Bergamo', 'di Codogno', 'in quarantena', 'un/a fascialattughe', 'miserabile', 'maleducato/a];
 
 var insult = '';
 
